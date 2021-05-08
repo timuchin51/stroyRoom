4a55a309311467d72dcd654309a71d310c44c5d4
timuchin51 <bezrodny83@gmail.com>
2021-05-08 22:26:30 +0300

    create django project

---
+++ stroyRoom/stroyRoom/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stroyRoom project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stroyRoom.settings')
+
+application = get_wsgi_application()
